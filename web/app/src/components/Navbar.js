8e274ae5fa40fa1b7e71a36bfe4d1ed9dfb2eaf4
Kornkanok Sattapong <Sattapong2546@gmail.com>
2024-07-11 16:48:08 +0700

    Update Navbar.js

--- web/app/src/components/Navbar.js
+++ web/app/src/components/Navbar.js
@@ -11,7 +11,7 @@ function Navbar() {
             <nav class="main-header navbar navbar-expand navbar-white navbar-light">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i>jiojjojojo</a>
                     </li>
                     
                 </ul>
