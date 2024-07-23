74c75b34849fe0ea0b4f524229f9ded2acaf52aa
najaGG <tor.saelao@gmail.com>
2024-07-24 00:47:44 +0700

    soket

--- api/server.js
+++ api/server.js
@@ -15,6 +15,7 @@ const ProductImageControll = require('./controllers/ProductImageControll');
 const BuyproductControll = require('./controllers/BuyproductControll');
 const DashboardControll = require('./controllers/DashboardControll');
 const OwnerControll = require('./controllers/OwnerControll');
+const TEST = require('./controllers/ProductControll copy');
 app.use(DashboardControll);
 app.use(BuyproductControll);
 app.use(ProductImageControll);
@@ -23,6 +24,35 @@ app.use(ProductControll);
 app.use(AdminControll);
 app.use(OwnerControll);
 
-app.listen(port,() =>{
+const socketIo = require('socket.io');
+const http = require('http');
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('sendMessage', (message) => {
+        if(message === 1){
+            console.log('1')
+        }else{
+            console.log('none')
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
+
+
+server.listen(port,() =>{
     console.log('listening on port ', port);
 })
