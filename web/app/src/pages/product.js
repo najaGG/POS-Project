c790e926e348230f6321907bc6a2bcb16b02f55d
Kornkanok-ctrl <142195228+Kornkanok-ctrl@users.noreply.github.com>
2024-07-25 16:24:24 +0700

    Update product.js
    ex.card

--- web/app/src/pages/product.js
+++ web/app/src/pages/product.js
@@ -1,25 +1,23 @@
 import Template from "../components/Template";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 function Product() {
     return (
         <>
             <Template>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="path/to/your/image.jpg" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <div className="card" style={{ width: 180 }}>
+                    <img src="placeholder.jpg" className="card-img-top" alt="Card image cap" />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </Template>
         </>
     );
 }
 
 export default Product;
+
+
+<img src="dist/img/logo-top.png" 
