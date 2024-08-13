89a60c3e357b0dcd81f97f9f95a9260b6bf2fc05
najaGG <tor.saelao@gmail.com>
2024-08-13 21:09:15 +0700

    Update coin.py

--- api/coin.py
+++ api/coin.py
@@ -1,17 +1,20 @@
 import sys
 from gpiozero import DigitalInputDevice
-from time import sleep
 
+
+relay = OutputDevice(21)
 coin = int(sys.argv[1])
 pin = 25
 device = DigitalInputDevice(pin, pull_up=False)
-
+print (coin)
 count = 0
+relay.on()
 while count < coin:
     value = device.value
     if value == 1:
         count += 10
-        print(1)  # พิมพ์ค่า 1 ทุกครั้งที่ค่าของ `value` เป็น 1
-    else:
-        print(0)
-    sleep(0.5)
+        print(1)
+        while device.value == 1:
+            pass
+if(count >= coin):
+    relay.off()
