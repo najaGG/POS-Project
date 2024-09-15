a8ba37ade0a2c7d7038af3b097e10ba687557b2d
Kornkanok-ctrl <142195228+Kornkanok-ctrl@users.noreply.github.com>
2024-09-15 15:11:13 +0700

    แก้ไขรูปภาพและดูภาพรวม

--- web/app/src/pages/Owner.js
+++ web/app/src/pages/Owner.js
@@ -72,7 +72,7 @@ function Home() {
     return (
         <>
             <div className="containers">
-                <div className="bg"></div>
+                <div className="kmitlpcc"></div>
                 <div className="form-container">
                     <div className="text-center mb-2" style={{ fontSize: 20 }}>Owner</div>
                     <div className="row">
